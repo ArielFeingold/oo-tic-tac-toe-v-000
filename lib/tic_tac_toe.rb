230e82e1f4186ad60629670b93feea7d9a707034
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-08 22:15:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,4 +71,29 @@ class TicTacToe
     end
   end
 
+    def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      position_1 = @board[win_combination[0]]
+      position_2 = @board[win_combination[1]]
+      position_3 = @board[win_combination[2]]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.all? do |pos|
+      pos == "X" || pos == "O"
+    end
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw? || full?
+  end
 end
